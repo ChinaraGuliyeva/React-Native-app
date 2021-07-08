04915d76884a65201b16e6dd7b5bb40308524a07
Chinara.Guliyeva <Chinara.Guliyeva@eurodesign.az>
2021-07-08 11:34:49 +0400

    Added heading, img and some styles

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { StyleSheet, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      <View style={styles.input_container}>
-        <TextInput placeholder="Что-то там" style={styles.user_input} />
-        <Button title="Добавить" />
+      <View>
+        <Text style={styles.heading}>App</Text>
+        <Image
+          style={styles.geo_img}
+          source={require('./assets/geolocation.png')
+          }
+        />
+        <Button
+          color='#295F88'
+          title="Get geolocation" />
       </View>
     </View>
   );
@@ -16,14 +23,16 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
-  input_container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+  heading: {
+    fontSize: 40,
+    textAlign: 'center',
+    padding: 10,
+    color: '#044476'
   },
-  user_input: { 
-    width: '50%', 
-    borderColor: 'black', 
-    borderWidth: 1, 
-    padding: 10 }
+  geo_img: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
+    borderRadius: 5
+  },
 })
