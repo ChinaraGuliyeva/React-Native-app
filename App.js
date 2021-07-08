96cc4effbc182db699d1546f5749a48ed3bda36f
Chinara.Guliyeva <Chinara.Guliyeva@eurodesign.az>
2021-07-08 10:42:08 +0400

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.input_container}>
+        <TextInput placeholder="Что-то там" style={styles.user_input} />
+        <Button title="Добавить" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
-});
+  input_container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  user_input: { 
+    width: '50%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10 }
+})
