3b8bf393de94e6f7913047bfb9b962e4b482dfb9
Chinara.Guliyeva <Chinara.Guliyeva@eurodesign.az>
2021-07-09 17:54:57 +0400

    Moved location to separate component

--- App.js
+++ App.js
@@ -1,50 +1,12 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, Image, TouchableOpacity } from 'react-native';
-import * as Location from 'expo-location';
+import React from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+import ShowLocation from './ShowLocation.js';
 
 export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  async function handleClick() {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      setErrorMsg('Permission to access location was denied');
-      return;
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    setLocation(location);
-  }
-
-  const clearLocation = () => {
-    setLocation(null)
-  }
-
   return (
     <View style={styles.screen}>
-      <View>
         <Text style={styles.heading}>Chinara's App</Text>
-        <Image
-          style={styles.geo_img}
-          source={require('./assets/geolocation.png')
-          }
-        />
-        <Button
-          color='#295F88'
-          title="Get geolocation"
-          onPress={handleClick} />
-        {location ?
-          <View style={styles.coordinates_container}>
-            <TouchableOpacity 
-              onPress={clearLocation}
-              style={styles.delete_coordinates}>
-              <Text style={styles.delete_coordinates_text}>X</Text>
-            </TouchableOpacity>
-            <Text style={styles.coordinates}>Lat: {location.coords.latitude}</Text>
-            <Text style={styles.coordinates}>Lng: {location.coords.longitude}</Text>
-          </View> : null}
-      </View>
+        <ShowLocation/>
     </View>
   );
 }
@@ -53,39 +15,10 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
-  coordinates: {
-    fontSize: 20
-  },
-  coordinates_container: {
-    marginTop: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#248F61',
-    borderRadius: 5
-  },
   heading: {
     fontSize: 40,
     textAlign: 'center',
     padding: 10,
     color: '#044476'
   },
-  geo_img: {
-    width: '100%',
-    height: 200,
-    marginBottom: 10,
-    borderRadius: 5
-  },
-  delete_coordinates: {
-    position: 'absolute',
-    right: 0,
-    borderWidth: 1,
-    borderColor: "#EC433D",
-    width: 25,
-    height: 25,
-    borderRadius: 50,            
-    
-  }, 
-  delete_coordinates_text: {
-    textAlign: "center"
-  }
 })
