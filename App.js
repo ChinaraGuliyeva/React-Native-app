cd5d891972247115a9819611b8c4bd373777c801
Chinara.Guliyeva <Chinara.Guliyeva@eurodesign.az>
2021-07-09 17:24:30 +0400

    Added deleting coordinates

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Button, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button, Image, TouchableOpacity } from 'react-native';
 import * as Location from 'expo-location';
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
-  async function handleClick(){
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+  async function handleClick() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  }
+
+  const clearLocation = () => {
+    setLocation(null)
   }
 
   return (
     <View style={styles.screen}>
       <View>
-        <Text style={styles.heading}>App</Text>
+        <Text style={styles.heading}>Chinara's App</Text>
         <Image
           style={styles.geo_img}
           source={require('./assets/geolocation.png')
@@ -28,13 +32,18 @@ export default function App() {
         />
         <Button
           color='#295F88'
-          title="Get geolocation" 
-          onPress={handleClick}/>
-        {location?  
-        <View style={styles.coordinates_container}>
-          <Text style={styles.coordinates}>Lat: {location.coords.latitude}</Text>
-          <Text style={styles.coordinates}>Lng: {location.coords.longitude}</Text>
-        </View>: null}
+          title="Get geolocation"
+          onPress={handleClick} />
+        {location ?
+          <View style={styles.coordinates_container}>
+            <TouchableOpacity 
+              onPress={clearLocation}
+              style={styles.delete_coordinates}>
+              <Text style={styles.delete_coordinates_text}>X</Text>
+            </TouchableOpacity>
+            <Text style={styles.coordinates}>Lat: {location.coords.latitude}</Text>
+            <Text style={styles.coordinates}>Lng: {location.coords.longitude}</Text>
+          </View> : null}
       </View>
     </View>
   );
@@ -47,12 +56,12 @@ const styles = StyleSheet.create({
   coordinates: {
     fontSize: 20
   },
-  coordinates_container:{
+  coordinates_container: {
     marginTop: 10,
     padding: 10,
     borderWidth: 1,
     borderColor: '#248F61',
-    borderRadius: 5  
+    borderRadius: 5
   },
   heading: {
     fontSize: 40,
@@ -66,4 +75,17 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 5
   },
+  delete_coordinates: {
+    position: 'absolute',
+    right: 0,
+    borderWidth: 1,
+    borderColor: "#EC433D",
+    width: 25,
+    height: 25,
+    borderRadius: 50,            
+    
+  }, 
+  delete_coordinates_text: {
+    textAlign: "center"
+  }
 })
